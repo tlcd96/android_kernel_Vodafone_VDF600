720192699a955275b0194913ea27bb9068e577ac
adomasalcore3 <adomasalcore3@users.noreply.github.com>
2017-01-26 09:06:05 -0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE="$WORKSPACE/toolchains/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-"
-export PATH="$PATH:$WORKSPACE/toolchains/arm-eabi-linaro-4.6.2/arm-eabi/bin"
+export CROSS_COMPILE=./toolchains/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-
+export PATH=$PATH:./toolchains/arm-eabi-linaro-4.6.2/arm-eabi/bin
 make msm8909-1gb_P809V50_defconfig
 #make menuconfig
 make -j4
